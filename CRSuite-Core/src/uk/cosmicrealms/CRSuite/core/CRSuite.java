b7d5359474109e74fc1c717aba1bcdbceef9b7cc
BuildTools <github@thomasred20.uk>
2015-07-24 12:59:22 +0100

    Added CRSuite-Core & CRSuite-Teleport

---
+++ CRSuite-Core/src/uk/cosmicrealms/CRSuite/core/CRSuite.java
@@ -0,0 +1,11 @@
+package uk.cosmicrealms.CRSuite.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Thomas on 24/07/2015.
+ */
+public class CRSuite extends JavaPlugin {
+    // This is the Main Connector Plugin for CRSuite, and will be used for all Communications between Plugins
+
+}
---
+++ CRSuite-Teleport/src/uk/cosmicrealms/CRSuite/teleport.java
@@ -0,0 +1,13 @@
+package uk.cosmicrealms.CRSuite;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Thomas on 24/07/2015.
+ */
+public class teleport extends JavaPlugin {
+    Plugin core = (Plugin) Bukkit.getServer().getPluginManager().getPlugin("CRSuite-Core");
+
+}
